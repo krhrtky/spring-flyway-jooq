0f11ee6c9107a2951d783040f7a5d1cfb7964eb6
t-kurihara <takuya.kurihara00@gmail.com>
2021-12-10 09:16:35 +0900

    Add Config setup jooq codegen.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.6.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("nu.studer.jooq") version "6.0.1"
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.spring") version "1.6.0"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
@@ -17,6 +18,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jooq")
+//    implementation("org.jooq:jooq-codegen:3.14.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.flywaydb:flyway-core")
@@ -25,7 +27,16 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    jooqGenerator("org.postgresql:postgresql:42.2.14")
 }
+
+//buildscript {
+//    configurations["classpath"].resolutionStrategy.eachDependency {
+//        if (requested.group == "org.jooq") {
+//            useVersion("3.14.0")
+//        }
+//    }
+//}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
@@ -36,4 +47,39 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+jooq {
+    configurations {
+        create("main") {
+            jooqConfiguration.apply {
+                logging = org.jooq.meta.jaxb.Logging.WARN
+                jdbc.apply {
+                    url = "jdbc:postgresql://localhost:5432/postgres"
+                    username = "postgres"
+                    password = ""
+                    driver = "org.postgresql.Driver"
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        inputSchema = "public"
+                        excludes = "flyway_schema_history"
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                    }
+                    target.apply {
+                        packageName = "nu.studer.sample"
+                        directory = "build/generated/jooq/main"
+                    }
+                    generator.name = "org.jooq.codegen.KotlinGenerator"
+                }
+            }
+        }
+    }
 }
